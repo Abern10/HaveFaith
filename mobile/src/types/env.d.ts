e7961009c82a089f213c7f66d3a3bbeda2fafd9b
Alex Bernatowicz <bernatowicz1122@gmail.com>
2025-05-15 14:04:18 -0500

    supabase env fix

--- mobile/src/types/env.d.ts
+++ mobile/src/types/env.d.ts
@@ -3,4 +3,6 @@ declare module '@env' {
   export const BIBLE_API_KEY: string;
   export const BIBLE_API_URL: string;
   export const DEFAULT_BIBLE_ID: string;
+  export const SUPABASE_URL: string;
+  export const SUPABASE_KEY: string;
 }
